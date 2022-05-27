2f958457e55503658f3baad532ffd417dc193209
Vinu Joseph <vinujoseph25@gmail.com>
2022-05-27 19:14:51 +0530

    logo change

--- components/_App/Navbar.js
+++ components/_App/Navbar.js
@@ -43,7 +43,7 @@ const Navbar = () => {
                         <nav className="navbar navbar-expand-lg navbar-light">
                             <Link href="/">
                                 <a className="navbar-brand">
-                                    <img src="/images/logo.png" alt="logo" />
+                                    <img src="/images/logo.png" width={100} alt="logo" />
                                 </a>
                             </Link>
 
